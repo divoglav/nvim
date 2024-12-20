117b2c9e809bf165187bf8de09d1121dfdfc9a4b
divoglav <divoglav@protonmail.com>
2024-12-21 01:18:35 +0200

    update

--- lua/plugins/kanagawa.lua
+++ lua/plugins/kanagawa.lua
@@ -10,6 +10,7 @@ return {
   config = function()
     require("kanagawa").setup({
       compile = false, -----------------------------------------------------------------------
+      --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
       -- NOTE: If you enable compilation, run :KanagawaCompile
       -- command every time you make changes to your config.
 
@@ -41,32 +42,33 @@ return {
         local theme = colors.theme
         local palette = colors.palette
         return {
-          -- Floats
+          -- Syntax --
+          Normal = { fg = palette.oldWhite },
+          Constant = { fg = palette.fujiWhite },
+          Boolean = { fg = palette.surimiOrange, bold = false },
+
+          ["@markup.heading"] = { fg = palette.sakuraPink, bold = true },
+          ["@markup.strong"] = { fg = palette.surimiOrange, bold = true },
+          ["@markup.italic"] = { fg = palette.springGreen, italic = true },
+          ["@markup.raw"] = { fg = palette.fujiWhite },
+          ["@markup.raw.markdown_inline"] = { fg = palette.fujiWhite, bg = palette.sumiInk4 },
+
+          -- Treesitter --
+          ["@lsp.typemod.function.readonly"] = { bold = false },
+
+          -- UI ---
           NormalFloat = { bg = "none" },
           FloatBorder = { bg = "none" },
           FloatTitle = { bg = "none" },
 
-          -- Windows
           NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
           LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
           MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
 
-          -- Popup Menu
           Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },
           PmenuSel = { fg = "none", bg = theme.ui.bg_p2 },
           PmenuSbar = { bg = theme.ui.bg_m1 },
           PmenuThumb = { bg = theme.ui.bg_p2 },
-
-          -- Syntax
-          Constant = { fg = palette.fujiWhite },
-          Boolean = { fg = palette.surimiOrange, bold = false },
-          -- Operator = { bold = true}
-
-          -- Treesitter
-          ["@lsp.typemod.function.readonly"] = { bold = false },
-          ["@markup.strong"] = { fg = palette.surimiOrange, bold = true, }
-
-
         }
       end,
     })
--- lua/plugins/tokyo.lua
+++ lua/plugins/tokyo.lua
@@ -2,5 +2,9 @@ return {
   "folke/tokyonight.nvim",
   lazy = false,
   priority = 1000,
-  opts = {},
+  config = function()
+    require("tokyonight").setup({
+      transparent = true,
+    })
+  end,
 }
